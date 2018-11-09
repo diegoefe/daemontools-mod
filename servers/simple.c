6cc0e3b7c1533a3f6725e702ecb4805e495d4bcc
Diego DB <diegoefe@gmail.com>
2018-11-08 21:20:43 -0300

    simple now prints his pid

--- servers/simple.c
+++ servers/simple.c
@@ -1,35 +1,35 @@
 #include <stdio.h>
 #include<stdio.h>
 #include<signal.h>
+#include<sys/types.h>
 #include<unistd.h>
 
 int aborted = 0;
 
-void sig_handler(int signo)
-{
-  if (signo == SIGINT) {
-    fprintf(stderr, "received SIGINT\n");
-    aborted = 1;
-  }
+void sig_handler(int signo) {
+	if(signo == SIGINT) {
+		fprintf(stderr, "received SIGINT\n");
+		aborted = 1;
+	}
 }
 
-int main(void)
-{
-  int salto=1;
-  if (signal(SIGINT, sig_handler) == SIG_ERR) {
-	  printf("\ncan't catch SIGINT\n");
-  }
-  // A long long wait so that we can easily issue a signal to this process
-  while(!aborted)  {
-	printf(".");
-	fflush(stdout);
-	if(0 == (salto % 50)) {
-		printf("\n");
-		salto = 0;
+int main(void) {
+	int salto=1;
+	if(signal(SIGINT, sig_handler) == SIG_ERR) {
+		printf("\ncan't catch SIGINT\n");
 	}
-	++salto;
-	usleep(100);
-  }
-  return 0;
+	printf("running simple server with PID: %d\n", getpid());
+
+	while(!aborted)  {
+		printf(".");
+		fflush(stdout);
+		if(0 == (salto % 50)) {
+			printf("\n");
+			salto = 0;
+		}
+		++salto;
+		sleep(1);
+	}
+	return 0;
 }
 
